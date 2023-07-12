a1f09a4a4cc9013f5a05c378f2e57d72d204a877
github_username <you@example.com>
2023-07-12 12:09:38 -0400

    Challenge 02

--- Ch02.py
+++ Ch02.py
@@ -4,9 +4,9 @@ import ping3
 import time
 import datetime
 
-# Script Name:                  Script Name
+# Script Name:                  Uptime Sensor Tool
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      00/00/0000
+# Date of latest revision:      07/11/2023
 # Purpose:                      In Python, create an uptime sensor tool that uses ICMP packets to evaluate
 #                               if hosts on the LAN are up or down.
 #                               
@@ -20,17 +20,23 @@ import datetime
 #                               - Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
 
 #Function
-#Single ICMP Packet to Specific IP Address every two second w/ response true=Up or false=Down
+#Single ICMP Packet to Specific IP Address
 def Single_ICMP():
     icmp_packet = '8.8.8.8'
     while True:
+
+        # Timestamp
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # response true=Up or false=Down
         response_status = 'Up' if ping3.ping(icmp_packet) else 'Down'
 
         # Print variable, timpestamp and destination ip tested
         print(f'[{timestamp}] Destination: {icmp_packet} | Status: {response_status}')
+        
+        # Request ping every two seconds
         time.sleep(2)
-
+# Execute conditional statement
 if __name__ == '__main__':
     Single_ICMP()
   
