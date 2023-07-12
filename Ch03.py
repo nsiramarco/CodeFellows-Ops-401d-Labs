af463d7236a8056ddb5cc2ca5a01cf000ca6bf63
github_username <you@example.com>
2023-07-12 19:28:18 -0400

    pending

--- Ch03.py
+++ Ch03.py
@@ -2,10 +2,20 @@
 import os
 
 
-# Script Name:                  
+# Script Name:                  Uptime Sensor Tool Part 2 of 2
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      07/11/2023
-# Purpose:                      
+# Date of latest revision:      07/12/2023
+# Purpose:                      In Python, add the below features to your uptime sensor tool.
+#                               
+#                               The script must:
+#                               
+#                               Ask the user for an email address and password to use for sending notifications.
+#                               Send an email to the administrator if a host status changes (from “up” to “down” or “down” to “up”).
+#                               Clearly indicate in the message which host status changed, the status before and after, and a timestamp of the event.
+#                               Important Notes
+#                               
+#                               DO NOT commit your email password in plain text within your script to GitHub as this easily becomes public.
+#                               Create a new “burner” account for this exercise. Do not use an existing email account.
 
 
 
