b2ef941b61e36ba4b02e034c7106f97f939df2e6
github_username <you@example.com>
2023-07-13 12:52:01 -0400

    Challenege 03

--- Ch03.py
+++ Ch03.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import os
+import smtpd
+import smtplib, time, datetime
+from getpass import getpass
 
 
 # Script Name:                  Uptime Sensor Tool Part 2 of 2
@@ -17,6 +20,89 @@ import os
 #                               DO NOT commit your email password in plain text within your script to GitHub as this easily becomes public.
 #                               Create a new “burner” account for this exercise. Do not use an existing email account.
 
+
+# Variables
+
+up = "The Ip address host is up!"
+down = "The Ip address host is down!"
+
+email = input("Enter Email: ")
+password = getpass("Enter Password: ")
+ip = input("Enter ip  to monitor")
+
+last = 0
+ping_result = 0
+
+# Function alert Up
+
+def Alertup():
+    now = datetime.datetime.now()
+    #SMTP sessiom
+    s = smtplib.SMTP(smtp.gmail.xom, '587')
+    #encryption
+    s.starttls()
+    #Start SMTP session
+    s.login(email, password)
+
+    message = "Server is up and running"
+    #Send email
+    s.sendmail("bot@codefellows.com", email, message)
+    #close 
+    s.quit()
+
+# Function Alert Down
+def Alertdown():
+    now = datetime.datetime.now()
+    #SMTP sessiom
+    s = smtplib.SMTP(smtp.gmail.xom, '587')
+    #encryption
+    s.starttls()
+    #Start SMTP session
+    s.login(email, password)
+
+    message = "Server is down"
+    #Send email
+    s.sendmail("bot@example.com", email, message)
+    #close 
+    s.quit()
+
+#Function for ping
+def ping_test():
+    now = datetime.datetime.now()
+
+    global ping_result
+    global last
+    
+    #ping status result
+    if ((ping_result != last) and (ping_result == up)):
+        #change value 
+        last = up
+        #send email
+        send_Alertup()
+
+    elif((ping_result != last) and (ping_result == down)):
+        #change value
+        last = down
+        #send email
+        send_Alertdown()
+
+    # send ping 
+    response = os.system("ping -c 1 " + ip)
+
+    # Evaluate ping
+    if response == 0:
+        ping_result = up
+    else:
+        ping_result = down
+    print(str(now) + ping_result + " to " + ip)
+
+
+# Main
+
+# While loop
+while True:
+    ping_test()
+    time.sleep(2)
 
 
 #END
