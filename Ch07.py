7ae03d74f15b7c43556837861a46b36bc6d02195
github_username <you@example.com>
2023-07-19 08:14:10 -0400

    pending

---
+++ Ch07.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+
+# Script Name:                  File Encryption Script Part 2 of 3
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      07/19/2023
+# Purpose:                      Add a feature capability to your script to:
+#                               
+#                               Recursively encrypt a single folder and all its contents.
+#                               Recursively decrypt a single folder that was encrypted by this tool.
+
+
+
+# Declaration of functions
+
+# Main
+
+# End
+
+Add a feature capability to your script to:
+
+Recursively encrypt a single folder and all its contents.
+Recursively decrypt a single folder that was encrypted by this tool.
