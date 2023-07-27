98282ffa8feba30c117edc47f748723644727a66
github_username <you@example.com>
2023-07-27 11:47:02 -0400

    pending

---
+++ Ch13.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+
+# Script Name:                  Network Security Tool with Scapy Part 3 of 3
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      07/27/2023
+# Purpose:                      The final iteration of your network scanning tool will perform the following:
+#                               
+#                               Ping an IP address determined by the user.
+#                                   If the host exists, scan its ports and determine if any are open.
+#                                   In Python, combine the two modes (port and ping) of your network scanner script.
+#                                   Eliminate the choice of mode selection.
+#                                   Continue to prompt the user for an IP address to target.
+#                                   Move port scan to its own function.
+#                                   Call the port scan function if the host is responsive to ICMP echo requests.
+#                                   Print the output to the screen.
+
+
+# Main
+
+# End
