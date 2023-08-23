18fcd42c214ff4aa20f63a3cdf46812fc25fde52
nsiramarco <natashasiebens@gmail.com>
2023-08-23 12:57:29 -0400

    Challenge 32

--- Ch32.py
+++ Ch32.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import os
+import hashlib
+import time
 import subprocess
 
 
-# Script Name:                  Signature-based Malware Detection Part 1 of 3
+# Script Name:                  Signature-based Malware Detection Part 2 of 3
 # Author:                       NATASHA SIRAMARCO
-# Date of latest revision:      08/21/2023
+# Date of latest revision:      08/22/2023
 # Purpose:                      Continue developing your Python malware detection tool.
 #                               
 #                               Alter your search code to recursively scan each file and folder in the user input directory path and print it to the screen.
@@ -21,11 +23,24 @@ import subprocess
 
 
 # Function
+# Function to generate MD5 hash
+def generate_md5(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
 def search_files(directory, filename):
     hits = []
-    for root, dirs, files in os.walk(directory):
-        if filename in files:
-            hits.append(os.path.join(root, filename))
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if filename in file:
+                file_path = os.path.join(root, file)
+                md5_hash = generate_md5(file_path)
+                file_size = os.path.getsize(file_path)
+                hits.append((file_path, md5_hash, file_size))
     return hits
 
 # Main
@@ -39,13 +54,19 @@ def main():
         # Search each file in the directory by name.
         hits = search_files(directory, filename)
         if hits:
-            # For each result found print to the screen the file name and location.
+            # For each result found print to the screen the file name, location, MD5 hash, file size, and timestamp.
             print("\nFiles found:")
             for hit in hits:
-                print(hit)
+                file_path, md5_hash, file_size = hit
+                timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+                print(f"Timestamp: {timestamp}")
+                print(f"File Name: {filename}")
+                print(f"File Size: {file_size} bytes")
+                print(f"File Path: {file_path}")
+                print(f"MD5 Hash: {md5_hash}\n")
             
             # Quantity of files searched and how many hits were found.
-            print("\nTotal files searched:", len(hits))
+            print("Total files searched:", len(hits))
             print("Total hits found:", len(hits))
         else:
             print("\nNo files found matching the specified name.")
@@ -58,3 +79,7 @@ if __name__ == "__main__":
 
 # END
 # classroom video and chatgpt assisted
+
+# Resources
+#           Hashlib Official Documentation - https://docs.python.org/3/library/hashlib.html
+#           Python Program to Find Hash of File - https://www.programiz.com/python-programming/examples/hash-file
