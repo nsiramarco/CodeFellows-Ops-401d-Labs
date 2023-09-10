16b453e96286bfb3089cf545b70b87f929080ef6
Natasha Siramarco <57773017+nsiramarco@users.noreply.github.com>
2023-09-10 15:25:23 -0400

    Challenge 44

---
+++ Ch44.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+timeout = # TODO: Set a timeout value here.
+sockmod.settimeout(timeout)
+
+hostip = # TODO: Collect a host IP from the user.
+portno = # TODO: Collect a port number from the user, then convert it to an integer data type.
+
+def portScanner(portno):
+    if sockmod.FUNCTION((hostip, portno)): # TODO: Replace "FUNCTION" with the appropriate socket.function call as found in the [socket docs](https://docs.python.org/3/library/socket.html)
+        print("Port closed")
+    else:
+        print("Port open")
+
+portScanner(port)
+
+
+# Resources
+#          Python socket module documentation - https://docs.python.org/3/library/socket.html
+#          DEMO.md - https://codefellows.github.io/ops-401-cybersecurity-guide/curriculum/class-44/challenges/DEMO.html
